aca49b5471d2b4982b8276212eb69c33b862e85b
MuhamadIhza <jambeuk@gmail.com>
2020-03-23 22:14:18 +0700

    inisiasi plant+pea
    Inisiasi plant

--- Plant.java
+++ Plant.java
@@ -12,16 +12,41 @@ public abstract class Plant {
         this.indexY = y;
     }
 
-    //public void attack();
+    public void attack(){
+        
+    }
+
+    public void setlife(int life){
+        this.life = life;
+    }
+    public int getlife(){
+        return this.life;
+    }
+
+    public int getX(){
+        return indexX;
+    }
+
+    public void setX(int x){
+        this.indexX = x;
+    }
+
+    public int getY(int y){
+        return indexY;
+    }
+
+    public void setY(int y){
+        this.indexY = y;
+    }
+
+    public void setLocPlant(int x,int y){
+        this.setX(x);
+        this.setY(y);
+    }
 
     //public void constantExcude();
 
     public void receiveAttack(Zombie z){
         this.life -= z.damage;
     }
-    
-    
-
-
-
 }
